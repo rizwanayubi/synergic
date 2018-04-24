371b593fdfb7b35733e9ff1d379415088befdc11
rizwanayubi <rizwan.ayubi296@gmail.com>
2018-04-24 18:17:58 +0500

    company profile

--- laravel_code/resources/views/users/demo.blade.php
+++ laravel_code/resources/views/users/demo.blade.php
@@ -11,44 +11,77 @@
 <div class="row">
     <div class="col-sm-12">
         <div clss="col-sm-12">
-            <img class="img" width="50" height="50" src="{{ asset('laravel_code/storage/app/public/user.png') }}" alt="Image" />
-            <h4>Rizwan Khan</h4>
+            <div class="left company_image">
+                <img class="img rounded-circle rounded" width="50" height="50" src="{{ asset('laravel_code/storage/app/public/user.png') }}" alt="Image" />
+            </div>
+                <p class="lead m-b-0"><b>Rizwan Khan</b></p>
+                <p class="text-muted font-13 m-t-0">FMC Company</p>
+            <div class="clearfix"></div>
         </div>
-        <div class="card-box table-responsive">
-            <h5>User Information</h5>
-            <hr>
-            <div class="col-sm-6">
-                <p>Date Of Joining</p>
-                <p>01-01-2018</p>
+        <div class="card-box primary_border_top">
+            <div class="col-sm-12">  
+                <div class="pull-left">  
+                    <h4 class="m-t-10 header-title">User Information</h4>
+                </div>
+                <div class="pull-right">  
+                    <!-- <h4 class="m-t-10 header-title">User Information</h4> -->
+                    <!-- <ul class="list-inline m-b-0 m-t-10">
+                        <li class="list-inline-item">
+                            Lorem ipsum
+                        </li>
+                        <li class="list-inline-item">
+                            Phasellus iaculis
+                        </li>
+                        <li class="list-inline-item">
+                            Nulla volutpat
+                        </li>
+                    </ul> -->
+                    <div class="btn-group m-b-0">
+                        <a href="#" class="btn btn-info-outline waves-effect waves-light btn-sm">Edit</a>
+                        <a href="#" class="btn btn-info-outline waves-effect waves-light btn-sm">Inactivate staff member</a>
+                        <a href="#" class="btn btn-info-outline waves-effect waves-light btn-sm">Block access</a>
+                    </div>
+                </div>
+               
+            </div>
+            <div class="col-sm-12">  
+                <hr>
             </div>
             <div class="col-sm-6">
-                <p>Email</p>
-                <p>abc@example.com</p>
-            </div>
-            <h5>Company Information</h5>
-            <hr>
-            <div class="col-sm-6">
-                <p>Name</p>
-                <p>Rizwan&Sons</p>
+                <p class="text-dark m-b-0 m-t-0">Date Of Joining</p>
+                <p class="text-muted m-b-10 font-13">01-01-2018</p>
             </div>
             <div class="col-sm-6">
-                <p>Contact</p>
-                <p>abc@example.com</p>
+                <p  class="text-dark m-b-0 m-t-0">Email</p>
+                <p class="text-muted m-b-10 font-13">abc@example.com</p>
+            </div>
+            <div class="col-sm-12">            
+                <h4 class="m-t-10 header-title">Company Information</h4>
+                <hr>
+            </div>
+
+            <div class="col-sm-6">
+                <p class="text-dark m-b-0 m-t-0">Company Name</p>
+                <p class="text-muted m-b-10 font-13">Rizwan&Sons</p>
             </div>
             <div class="col-sm-6">
-                <p>Office Address</p>
-                <p>68-T Gulberg ||</p>
+                <p class="text-dark m-b-0 m-t-0">Contact #</p>
+                <p class="text-muted m-b-10 font-13">(971) 000-000 00</p>
             </div>
             <div class="col-sm-6">
-                <p>Billing Address</p>
-                <p>68-T Gulberg ||</p>
+                <p class="text-dark m-b-0 m-t-0">Office Address</p>
+                <p class="text-muted m-b-10 font-13">68-T Gulberg ||</p>
             </div>
             <div class="col-sm-6">
-                <p>Company License</p>
-                <p></p>
+                <p class="text-dark m-b-0 m-t-0">Billing Address</p>
+                <p class="text-muted m-b-10 font-13">68-T Gulberg ||</p>
+            </div>
+            <div class="col-sm-12">
+                <h4 class="m-t-10 header-title">Company License</h4>
+                <hr>
                 <img class="img" width="50" height="50" src="{{ asset('laravel_code/storage/app/public/user.png') }}" alt="Image" />
-                </p>
             </div>
+            <div class="clearfix"></div>
         </div>
     </div>
 </div>
